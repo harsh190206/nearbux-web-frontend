07e168a6e58ad04e22abf3b8a1de06f43925e1ee
anku-26 <2654anku@gmail.com>
2025-04-27 01:41:16 +0530

    incomp_signup_page

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    
+    <div className='h-screen bg-red-600  flex w-screen'>
+      <div className='w-1/2 bg-[#4184dd]'></div>
+      <div className='w-1/2 bg-[#FFFFFF]'>
+      <div className='border-2 rounded-xl h-9/10 w-9/10  mt-6  m-auto'>
+
+      <p className=' h-14 text-xl font-bold  font-Montserrat'> let's get you started</p>
+      <br>
+      </br>
+      <p className=''> Create a new account</p>
+
+
+
+      </div>
+      
+      </div>
+    
+
+      
+    </div>
+  )
+}
+
+export default App
